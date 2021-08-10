1e4ba979d1d803a17d91a549a0cfdd342f7ee25a
Irena <iryna.morozova87@gmail.com>
2021-08-10 21:52:18 +0200

    index.js

--- webpack.config.cjs
+++ webpack.config.cjs
@@ -8,6 +8,9 @@ const mode = process.env.NODE_ENV || 'development';
 
 module.exports = {
   mode,
+  entry: [
+    `${__dirname}/src/index.js`,
+  ],
   resolve: {
     extensions: ['.js', '.jsx'],
   },
